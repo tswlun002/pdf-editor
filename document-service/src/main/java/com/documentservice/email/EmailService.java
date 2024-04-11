fcfbc93f9f665a9a601662d8301c131d3de147b6
Lunga Tsewu <1998tsewu@gmail.com>
2024-04-11 22:47:45 +0200

    removed redundent catch block

--- document-service/src/main/java/com/documentservice/email/EmailService.java
+++ document-service/src/main/java/com/documentservice/email/EmailService.java
@@ -9,6 +9,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+<<<<<<< HEAD
+=======
+import org.springframework.web.client.RestTemplate;
+>>>>>>> 6fc2089 (removed redundent catch block)
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,9 +29,25 @@ public class EmailService implements  IEmail {
     private @Valid Boolean getResponse(PDF pdf) {
       var response=false;
         try {
+<<<<<<< HEAD
             response=producer.sendDownloadEvent(new DownLoadDocumentEvent(pdf.getEmail(), pdf.getImage()));
         } catch (Exception e) {
                 log.info("Error:-----------> message:{},status code:{}", e.getMessage(),500,e);
+=======
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+            MultiValueMap<String, Object> body
+                    = new LinkedMultiValueMap<>();
+            body.add("pdf",  pdf.getImage());
+            body.add("email", new EmailRequest(pdf.getEmail()));
+            HttpEntity<MultiValueMap<String, Object>> requestEntity
+                    = new HttpEntity<>(body, headers);
+            response = restTemplate.postForEntity("http://localhost:8081/pdf-editor/email/send",requestEntity, String.class);
+
+        } catch (Exception e) {
+                log.info("Error:-----------> message:{},status code:{}", e.getMessage(),500);
+>>>>>>> 6fc2089 (removed redundent catch block)
                 throw new InternalServerError("Internal server error.");
         }
         return response;
