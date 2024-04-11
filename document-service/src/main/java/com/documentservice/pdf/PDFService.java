462640c6e4ea88ca736fbda898ae88753ecdfe6a
Lunga Tsewu <1998tsewu@gmail.com>
2024-04-11 22:42:25 +0200

    A moved all the classes related to document creation to document service

--- document-service/src/main/java/com/documentservice/pdf/PDFService.java
+++ document-service/src/main/java/com/documentservice/pdf/PDFService.java
@@ -2,8 +2,11 @@ package com.documentservice.pdf;
 
 import com.documentservice.exception.InternalServerError;
 import com.documentservice.exception.InvalidDocument;
+<<<<<<< HEAD
 import com.documentservice.exception.InvalidUser;
 import com.documentservice.user.User;
+=======
+>>>>>>> 8452314 (moved all the classes related to document creation to document service)
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,7 @@ import java.util.Optional;
 @Slf4j
 public class PDFService implements IPDF{
     private final   PDFRepository repository;
+<<<<<<< HEAD
     private final User user;
 
     @Override
@@ -26,6 +30,15 @@ public class PDFService implements IPDF{
         try{
 
             var doc = repository.findByEmail(userDto.getEmail())
+=======
+    @Override
+    public boolean saveDocument(UserDocument userDocument) throws InvalidDocument {
+        if(userDocument==null)throw new InvalidDocument("Document try to save is invalid");
+
+        var isSaved =false;
+        try{
+            var doc = repository.findByEmail(userDocument.email())
+>>>>>>> 8452314 (moved all the classes related to document creation to document service)
                     .stream().filter(d->d.getName().equals(userDocument.name()))
                     .toList();
 
--- user-service/src/main/java/com/userservice/pdffile/PdfDownloadRequest.java
+++
@@ -1,6 +0,0 @@
-package com.userservice.pdffile;
-
-public record PdfDownloadRequest(
-
-) {
-}
