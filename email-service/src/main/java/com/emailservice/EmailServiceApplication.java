6e3a8a862c425436ad161954ec5d0e1a4301d456
CP373931 <LungaTsewu@capitecbank.co.za>
2024-06-04 15:15:36 +0200

    Added email service and document service as discovery clients

--- email-service/src/main/java/com/emailservice/EmailServiceApplication.java
+++ email-service/src/main/java/com/emailservice/EmailServiceApplication.java
@@ -2,12 +2,14 @@ package com.emailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmailServiceApplication
 {
     public static void main( String[] args )
