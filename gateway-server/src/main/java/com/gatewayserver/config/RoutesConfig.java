7918ee45da3274a4046512cca01c8cbe9c39ef69
CP373931 <LungaTsewu@capitecbank.co.za>
2024-06-10 11:20:57 +0200

    Added gateway server to manage routing requests and filter request and responses

---
+++ gateway-server/src/main/java/com/gatewayserver/config/RoutesConfig.java
@@ -0,0 +1,26 @@
+package com.gatewayserver.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutesConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes().
+                route(predicateSpec -> predicateSpec.path("/pdf-editor/users/**").
+                        filters(filterSpec->filterSpec.rewritePath("/pdf-editor/users/(?<segment>.*)","/pdf-editor/users/${segment}"))
+                        .uri("lb://USERS"))
+                .route(predicateSpec -> predicateSpec.path("/pdf-editor/documents/**").
+                        filters(filterSpec->filterSpec.rewritePath("/pdf-editor/documents/(?<segment>.*)","/pdf-editor/documents/${segment}"))
+                        .uri("lb://DOCUMENTS"))
+                .route(predicateSpec -> predicateSpec.path("/pdf-editor/emails/**").
+                        filters(filterSpec->filterSpec.rewritePath("/pdf-editor/emails/(?<segment>.*)","/pdf-editor/email/${segment}"))
+                        .uri("lb://EMAIL"))
+
+                .build();
+    }
+}
